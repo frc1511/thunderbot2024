75b2353dff893958b3d3daa623282c6b5273e0cc
Calla <24cdouglass@gmail.com>
2024-02-10 15:00:41 -0600

    update io map.h based on the updated wookie

--- src/main/include/Basic/IOMap.h
+++ src/main/include/Basic/IOMap.h
@@ -1,5 +1,16 @@
 #pragma once
 
+
+/*
+    ██╗ ██████╗     ███╗   ███╗ █████╗ ██████╗ 
+    ██║██╔═══██╗    ████╗ ████║██╔══██╗██╔══██╗
+    ██║██║   ██║    ██╔████╔██║███████║██████╔╝
+    ██║██║   ██║    ██║╚██╔╝██║██╔══██║██╔═══╝ 
+    ██║╚██████╔╝    ██║ ╚═╝ ██║██║  ██║██║     
+    ╚═╝ ╚═════╝     ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝     
+*/
+
+
 #define CAN_SWERVE_DRIVE_FR 2
 #define CAN_SWERVE_DRIVE_FL 3
 #define CAN_SWERVE_DRIVE_BR 4
@@ -14,6 +25,15 @@
 #define CAN_SWERVE_CANCODER_FL 11
 #define CAN_SWERVE_CANCODER_BR 12
 #define CAN_SWERVE_CANCODER_BL 13
+
+#define CAN_INTAKE_MOTOR 15
+#define CAN_SHOOTER_LEFT_MOTOR 16
+#define CAN_SHOOTER_RIGHT_MOTOR 17
+#define CAN_ARM_PIVOT_MOTOR 18
+#define CAN_ARM_BRAKE_MOTOR 19
+
+#define CAN_HANG_ARM_RIGHT 20
+#define CAN_HANG_ARM_LEFT 21
 
 
 //number for this isnt actually on IOMap yet so change as needed based on what makes sense once we've finalized sensors more
