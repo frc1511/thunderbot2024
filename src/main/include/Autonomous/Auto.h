e6c70ab5ba314a78873e8bc5c1c605cba81a0cfd
Calla <>
2024-03-03 18:18:01 -0500

    Working Auto

--- src/main/include/Autonomous/Auto.h
+++ src/main/include/Autonomous/Auto.h
@@ -25,7 +25,11 @@ private:
         DO_NOTHING   = 0,
         SPEAKER_1_GP = 1,
         SPEAKER_2_GP = 2,
-        TEST         = 3,
+        HAVOC        = 3,
+        BASIC_LOC_1  = 4,
+        BASIC_LOC_2  = 5,
+        BASIC_LOC_3  = 6,
+        TEST         = 7
     };
     Drive *drive;
     Shamptake *shamptake;
@@ -40,11 +44,20 @@ private:
     void test();
     void speaker1();
     void speaker2();
+    void havoc();
+    void basic_loc_1();
+    void basic_loc_2();
+    void basic_loc_3();
+
     const std::map<AutoMode, const char*> autoModeNames {
         { AutoMode::DO_NOTHING,     "Do Nothing"        },
         { AutoMode::SPEAKER_1_GP,   "1 GP Speaker"      },
         { AutoMode::SPEAKER_2_GP,   "2 GP Speaker"      },
-        { AutoMode::TEST,           "Test"              },
+        { AutoMode::HAVOC,          "Hail Mary"         },
+        { AutoMode::BASIC_LOC_1,    "Shamptake Loc 1"   },
+        { AutoMode::BASIC_LOC_2,    "Shamptake Loc 2"   },
+        { AutoMode::BASIC_LOC_3,    "Shamptake Loc 3"   },
+        { AutoMode::TEST,           "Test"              }
 
     };
     int step = 0;
@@ -52,14 +65,26 @@ private:
     enum class Path {
         SPEAKER_1,
         SPEAKER_2_STAGE_2,
+        HAVOC,
+        BASIC_LOC_1,
+        BASIC_LOC_2,
+        BASIC_LOC_3
     };
     const std::map<Path, CSVTrajectory> bluePaths {
         { Path::SPEAKER_1,          CSVTrajectory{ DEPLOY_DIR "ThunderAuto/speaker_1.csv",          false } },
-        { Path::SPEAKER_2_STAGE_2,  CSVTrajectory{ DEPLOY_DIR "ThunderAuto/speaker_2_stage_2.csv",  false } }
+        { Path::SPEAKER_2_STAGE_2,  CSVTrajectory{ DEPLOY_DIR "ThunderAuto/speaker_2_stage_2.csv",  false } },
+        { Path::HAVOC,              CSVTrajectory{ DEPLOY_DIR "ThunderAuto/havoc.csv",              false } },
+        { Path::BASIC_LOC_1,        CSVTrajectory{ DEPLOY_DIR "ThunderAuto/basic_pickup_1.csv",     false } },
+        { Path::BASIC_LOC_2,        CSVTrajectory{ DEPLOY_DIR "ThunderAuto/basic_pickup_2.csv",     false } },
+        { Path::BASIC_LOC_3,        CSVTrajectory{ DEPLOY_DIR "ThunderAuto/basic_pickup_3.csv",     false } }
     };
     const std::map<Path, CSVTrajectory> redPaths {
         { Path::SPEAKER_1,          CSVTrajectory{ DEPLOY_DIR "ThunderAuto/speaker_1.csv",          true  } },
         { Path::SPEAKER_2_STAGE_2,  CSVTrajectory{ DEPLOY_DIR "ThunderAuto/speaker_2_stage_2.csv",  true  } },
+        { Path::HAVOC,              CSVTrajectory{ DEPLOY_DIR "ThunderAuto/havoc.csv",              true  } },
+        { Path::BASIC_LOC_1,        CSVTrajectory{ DEPLOY_DIR "ThunderAuto/basic_pickup_1.csv",     true  } },
+        { Path::BASIC_LOC_2,        CSVTrajectory{ DEPLOY_DIR "ThunderAuto/basic_pickup_2.csv",     true  } },
+        { Path::BASIC_LOC_3,        CSVTrajectory{ DEPLOY_DIR "ThunderAuto/basic_pickup_3.csv",     true  } }
     };
     const std::map<Path, CSVTrajectory>* paths = nullptr;
 
